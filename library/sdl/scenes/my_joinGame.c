15cdab5cae47bbab440b8385a0057d1d05e903aa
vignan_q <vignan_q@etna-alternance.net>
2021-04-23 12:24:20 +0000

    graphical

--- library/sdl/scenes/my_joinGame.c
+++ library/sdl/scenes/my_joinGame.c
@@ -9,22 +9,26 @@
 
 static void my_refreshJoinGame(SDL_Renderer* renderer) {
     my_clearWindows(renderer);
-    SDL_Rect buttonBack[2] = {{15, 15, 100, 50 },
-                              {40 , 25, 50,  30}};
-    SDL_Rect buttonPlay[2] = {{30,  500, 250, 75},
-                              {130, 525, 50,  30}};
-
-    my_drawRectangle(renderer, buttonBack[0], 150, 0, 0);
-    my_drawRectangle(renderer, buttonPlay[0], 0, 150, 0);
-
-    my_drawText(renderer, buttonBack[1], 0, 0, 0, "Back");
-    my_drawText(renderer, buttonPlay[1], 0, 0, 0, "Play");
+    SDL_Rect buttonBack = {430 , 525, 50,  30};
+    SDL_Rect buttonPlay = {130, 525, 50,  30};    
+    SDL_Rect playPos = {230 , 500 , 50 , 75};
+    for (int i = 0; i < 5; i++) {
+        my_drawImage(renderer, playPos , "./library/assets/block/block (32).bmp");
+        playPos.x = playPos.x - 50;
+    }
+    SDL_Rect backPos = {530 , 500 , 50 , 75};
+    for (int i = 0; i < 5; i++) {
+        my_drawImage(renderer, backPos , "./library/assets/block/block (30).bmp");
+        backPos.x = backPos.x - 50;
+    }
 
     SDL_Rect blockPos = {550 , 625 , 50 , 75};
     for (int i = 0; i < 12; i++) {
         my_drawImage(renderer, blockPos , "./library/assets/block/block (75).bmp");
         blockPos.x = blockPos.x - 50;
     }
+    my_drawText(renderer, buttonBack , 0, 0, 0, "Back");
+    my_drawText(renderer, buttonPlay , 0, 0, 0, "Play");
     SDL_RenderPresent(renderer);
 }
 
@@ -43,8 +47,8 @@ int my_joinGame(SDL_Window *window, SDL_Renderer *renderer) {
                     my_exitWindows(window, renderer);
                     return 0;
                 case SDL_MOUSEBUTTONDOWN:
-                    if (event.button.x >= 15 && event.button.x <= 15 + 100 && event.button.y >= 15 &&
-                        event.button.y <= 15 + 50) {
+                    if (event.button.x >= 280 && event.button.x <= 280 + 250 && event.button.y >= 500 &&
+                        event.button.y <= 500 + 75) {
                         return 1;
                     } else if (event.button.x >= 30 && event.button.x <= 30 + 250 &&
                                event.button.y >= 500 &&
